cc0457b00bb66e2d202df7826912b2be7df4cc63
Enzzo <enzzokenzo@gmail.com>
2026-03-04 19:36:35 -0300

    style and js

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-const divNoLogin = document.getElementById("semloginContainer");
-const buttonLogin = document.getElementById("loginButton");
+const divNoLogin = document.getElementById("semloginContainer")
+const buttonLogin = document.getElementById("loginButton")
+const h1 = document.getElementById("h1")
 
 buttonLogin.addEventListener("click", function(){
     if (divNoLogin.classList.contains("open")){
-        divNoLogin.classList.remove("open");
-        divNoLogin.classList.add("close");
+        divNoLogin.classList.remove("open")
+        divNoLogin.classList.add("close")
+        h1.classList.add("fadeIn")
+        h1.style.textAlign = "end"
+        setTimeout(function(){h1.classList.remove("fadeIn")}, 1000)
+        h1.innerText = "Ainda não tem cadastro?"
+        buttonLogin.innerText = "Cadastre-se agora!"
     }
     else{
-        divNoLogin.classList.remove("close");
-        divNoLogin.classList.add("open");
+        divNoLogin.classList.remove("close")
+        divNoLogin.classList.add("open")
+        h1.classList.add("fadeIn")
+        h1.style.textAlign = "start"
+        setTimeout(function(){h1.classList.remove("fadeIn")}, 1000)
+        h1.innerText = "Já possui cadastro?"
+        buttonLogin.innerText = "Entrar em sua conta!"
     }
 });
